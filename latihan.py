3417d778ebd6c03fc7e5ee9b0efb429e1d64be10
VeonN4 <fxzia13@gmail.com>
2024-08-06 22:22:13 +0700

    Initial commit

---
+++ latihan.py
@@ -0,0 +1,15 @@
+print('Hello world')
+print(69 * 69)
+print('''
+Aku sayang kamu juga sayang tapi sayang kita berjauhan
+tahan-tahan ku tahan semalam
+''')
+
+import random
+a = random.randint(1,100)
+
+print(f"""
+Nama\t:  Muhammad Razfaziya Adinata
+Usia\t:  16 tahun
+Sigma\t:  {a}%
+""")
