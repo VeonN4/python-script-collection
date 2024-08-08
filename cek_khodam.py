b8f2377753b1a7cbad162b125d064cb4b90b5d8d
VeonN4 <fxzia13@gmail.com>
2024-08-08 14:50:33 +0700

    Added cek_khodam.py and modified mesinATM.py

---
+++ cek_khodam.py
@@ -0,0 +1,15 @@
+import random
+
+print("="*30)
+print("\tCEK KHODAM")
+print("="*30)
+
+user = input("Masukan nama: ")
+
+khodam1     =   ["Monyet", "Kucing", "Anjing", "Siluman", "Motor", "Knalpot", "Kodok", "Goblin"]
+khodam2     =   ["Pemalas", "Julid", "Hideung", "Mio", "Sawah", "Vario", "Asli Sunda"]
+
+resultKhodam1 = random.choice(khodam1)
+resultKhodam2 = random.choice(khodam2)
+
+print(f"Khodam {user} adalah {resultKhodam1} {resultKhodam2}")
--- mesinATM.py
+++ mesinATM.py
@@ -1,8 +1,9 @@
 class User:
-    def __init__(self, name, pin, uang):
+    def __init__(self, name, pin, uang, status):
         self.name   = name
         self.pin    = pin
         self.uang   = uang
+        self.status = status
 
     def increaseUang(self, uang, inputUang):
         self.uang += inputUang
@@ -11,9 +12,20 @@ class User:
         self.pin = newPin
 
 def login(user):
-    username = str(input("Masukan nama: "))
+    username = input("Masukan Nama: ")
 
-    if username == 
+    confirmed = 0
+
+    for i in range(len(user)):
+        a = user[i].name
+        if a == username:
+            confirmed = 1
+            break
+
+    if not a == username:
+        print("Try again.")
+
+    return confirmed
 
 def display_menu():
     menu = ["Deposit", "Tarik", "Change PIN", "Exit"]
@@ -30,8 +42,6 @@ def display_menu():
 def deposit(users):
     print("\tMenu Deposit")
 
-    
-
 def tarik(uang):
     pass
 
@@ -39,24 +49,30 @@ def changePIN(pin):
     pass
 
 def main():
-    users = {
-        User("Hendri", 6969, 9999999999999999),
-        User("Asep", 5382, 7992000),
-        User("Yudi", 2331, 20000000)
-    }
+    users = [
+        User("Hendri", 6969, 9999999999999999, False),
+        User("Asep", 5382, 7992000, False),
+        User("Yudi", 2331, 20000000, False)
+    ]
 
     choice = 0
 
     while True:
-        choice = display_menu()
-        if choice == 1:
-            deposit(users)
-        elif choice == 2:
-            tarik()
-        elif choice == 3:
-            changePIN()
-        else:
-            exit()
+        loginCheck = login(users)
+        if loginCheck == 1:
+            break
+
+    if loginCheck == 1:
+        while True:
+            choice = display_menu()
+            if choice == 1:
+                deposit(users)
+            elif choice == 2:
+                tarik()
+            elif choice == 3:
+                changePIN()
+            else:
+                exit()
 
 if __name__ == "__main__":
     main()
