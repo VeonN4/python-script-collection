022a5f3264faf8c029e089e232abf964af336fda
VeonN4 <fxzia13@gmail.com>
2024-08-09 13:36:40 +0700

    Fixed rumus kubus

--- Geometri/kubus.py
+++ Geometri/kubus.py
@@ -5,6 +5,7 @@ print("="*30)
 angka = int(input("Masukan nilai sisi\t: "))
 
 luas = 6 * angka
+Volume =  angka**3
 
 print('Luas Kubus\t\t:', luas, "cm2")
-print('Volume Kubus\t\t:', angka, "cm2")
+print('Volume Kubus\t\t:', Volume, "cm2")
