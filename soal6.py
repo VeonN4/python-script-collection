b56344977fc16890acc199d25e66b70164c88a23
VeonN4 <fxzia13@gmail.com>
2024-08-19 20:43:27 +0700

    Added soal python

---
+++ soal6.py
@@ -0,0 +1,14 @@
+print("="*30)
+print("PROGRAM LINGKARAN")
+print("="*30)
+
+jari_jari = int(input("Masukan jari-jari: "))
+
+phi = 3.14
+
+hasilLuas = phi * jari_jari * jari_jari
+hasilKeliling = phi * 2 * jari_jari
+
+print("Luas:", hasilLuas, "cm2")
+print("Keliling:", hasilKeliling, "cm")
+
