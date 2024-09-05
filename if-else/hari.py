8c6b4670437547eeff68645f7e28feefbf174bb3
VeonN4 <fxzia13@gmail.com>
2024-09-05 09:24:41 +0700

    Fixed hari.py

--- if-else/hari.py
+++ if-else/hari.py
@@ -1,32 +1,55 @@
-print("Menghitung hari dengan sigma")
+print("\nMenghitung hari dengan sigma\n")
 
-#inputYear = int(input("Masukan tahun: "))
-#inputBulan = int(input("Masukan bulan: "))
+inputYear = int(input("Masukan tahun: "))
+inputBulan = input("Masukan bulan: ")
 
-month = {
-    "Januari": 31,
-    "Febuari": 28,
-    "Maret": 31,
-    "April": 30,
-    "Mei": 31,
-    "Juni": 30,
-    "Juli": 31,
-    "Agustus": 30,
-    "September": 31,
-    "Oktober": 30,
-    "November": 31,
-    "Desember": 30
+if any(char.isdigit() for char in inputBulan):
+    bulan = int(inputBulan)
+else:
+    bulan = inputBulan
+
+months = {
+    "januari": 31,
+    "febuari": 28,
+    "maret": 31,
+    "april": 30,
+    "mei": 31,
+    "juni": 30,
+    "juli": 31,
+    "agustus": 30,
+    "september": 31,
+    "oktober": 30,
+    "november": 31,
+    "desember": 30
 }
 
+# Fungsi untuk mengecek tahun kabisat atau tidak
 def cekTahun(year):
     if year % 400 == 0 or year % 4 == 0:
-        print(f"{2024} adalah tahun kabisat")
-        month["Febuari"] += 1
+        print(f"\n{year} adalah tahun kabisat")
+        months["febuari"] += 1
     elif year % 100 == 0 or year % 100 != 0 or year % 400 != 0 or year % 4 != 0:
-        print(f"{year} Bukanlah tahun kabisat")
+        print(f"\n{year} Bukanlah tahun kabisat")
 
-idx for idx, key in :
-    print(month)
+    return year # This is the most dumbest thing i ever do. Thank you!
+
+# Fungsi untuk mengecek bulan dan mengeluarkan ada berapa hari dalam bulan tersebut, dengan input bulan dalam huruf ataupun angka.
+def cekBulan(month):
+    if isinstance(month, int):
+        monthKey = list(months.keys())[int(month)-1]
+        monthValue = list(months.values())[int(month)-1]
+    elif isinstance(month, str):
+        monthKey = list(months.keys()).index(month.lower())
+        monthValue = list(months.values())[monthKey]
+        
+        monthKey = list(months.keys())[monthKey] # I got no idea what i'm doing in this 4 lines of codes, but it works though. I won't touch it again.
+
+    return monthKey, monthValue
+
+year = cekTahun(inputYear)
+monthKey, monthValue = cekBulan(bulan)
+
+print("Tahun", year, "Bulan", monthKey, "memiliki", monthValue, "hari\n")
 
 #cekTahun(inputYear)
-print("Bulan", month)
+#print("Bulan", month)
