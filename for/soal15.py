c1ec7bd34881d5e0a3b68c7d50a5ec823e3f6bad
VeonN4 <fxzia13@gmail.com>
2024-10-11 21:59:15 +0700

    pengulangan

---
+++ for/soal15.py
@@ -0,0 +1,5 @@
+panjang = 10
+
+for x in range(panjang):
+    print(" " * (panjang - x) + "*" * (2 * x - 1))
+
--- pengulangan/soal15.py
+++
@@ -1,4 +0,0 @@
-panjang = 10
-
-for x in range(panjang):
-    print(" " * (panjang - x) + "*" * (2 * x -1))
