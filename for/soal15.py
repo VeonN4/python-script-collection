0b586588bc7746003f276789523590538aba1fcc
VeonN4 <fxzia13@gmail.com>
2024-10-28 14:50:01 +0700

    finish for loop

--- for/soal15.py
+++ for/soal15.py
@@ -1,5 +1,8 @@
-panjang = 10
+perkalian = int(input("Masukan perkalian: "))
 
-for x in range(panjang):
-    print(" " * (panjang - x) + "*" * (2 * x - 1))
+print('x', end="\t")
+print('\t'.join(str(i) for i in range(1, perkalian+1)))
 
+for r in range(1,perkalian+1):
+    print(r, end='\t')
+    print('\t'.join(str(r*c) for c in range(1, perkalian+1)))
--- for/soal18.py
+++ for/soal18.py
@@ -1,1 +1,15 @@
-for x in range
+x = 1
+while x <= 12:
+    print(x, end="\t")
+    x += 1
+
+y = 1
+while y <= 12:
+    print("")
+    print(y, end='\t')
+    z = 1
+    while z <= 12:
+        print(y*z, end='\t')
+        z += 1
+    y += 1
+print()
