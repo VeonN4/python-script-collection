1dbde74978489e1613817293533575f822d7e831
VeonN4 <fxzia13@gmail.com>
2024-11-14 12:35:15 +0700

    Completed Kertas Gunting Batu Game

--- Project/kertas_gunting_batu.py
+++ Project/kertas_gunting_batu.py
@@ -1,10 +1,72 @@
 import random
 
+userScore = 0
+botScore = 0
 
-choice = random.choice("Kertas", "Gunting", "Batu")
-userChoice = str(input("(Kertas, Gunting, Batu): "))
+winCondition = 0
 
-if choice == userChoice:
-    print("Draw")
-elif userChoice == "Batu" and choice == "kertas":
-    print("You lose")
+def userPrompt():
+    print("\nGame")
+    print("Kertas - Gunting - Batu")
+
+    userChoice = input("Pilih (Kertas, Gunting, Batu): ")
+
+    return userChoice
+
+
+def game(userChoice: str, botChoice: str):
+    if userChoice.lower().strip() == "kertas":
+        match botChoice:
+            case "Kertas":
+                winCondition = 1
+            case "Batu":
+                winCondition = 0
+            case "Gunting":
+                winCondition = 0
+    elif userChoice.lower().strip() == "gunting":
+        match botChoice:
+            case "Kertas":
+                winCondition = 0
+            case "Batu":
+                winCondition = 1
+            case "Gunting":
+                winCondition = 0
+    elif userChoice.lower().strip() == "batu":
+        match botChoice:
+            case "Kertas":
+                winCondition = 0
+            case "Batu":
+                winCondition = 0
+            case "Gunting":
+                winCondition = 1
+
+    return winCondition
+
+def setScore(condition: int):
+    global userScore
+    global botScore
+
+    if condition == 1:
+        print("Kamu menang!\n")
+        userScore += 1
+    else:
+        print("Kamu kalah!\n")
+        botScore += 1
+
+    print(f"Bot Score: {botScore}")
+    print(f"User Score: {userScore}")
+
+def main():
+    choices = ["Kertas", "Gunting", "Batu"]
+    userChoice = ""
+
+    while userChoice.lower().strip() != "stop":
+        botChoice = random.choice(choices)
+        userChoice = userPrompt()
+        condition = game(userChoice, botChoice)
+        setScore(condition)
+
+if __name__ == "__main__":
+    main()
+
+        
